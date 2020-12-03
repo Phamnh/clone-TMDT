4d2b033cf88215daabed3edb8f8e00420c688cdd
JeusT0910 <72457937+JeusT0910@users.noreply.github.com>
2020-12-03 16:51:38 +0700

    0312

--- DoAnAsp/Migrations/20201203094726_InitialCreate.Designer.cs
+++ DoAnAsp/Migrations/20201203094726_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoAnAsp.Migrations
 {
     [DbContext(typeof(DPContext))]
-    [Migration("20201203085745_InitialCreate")]
+    [Migration("20201203094726_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
