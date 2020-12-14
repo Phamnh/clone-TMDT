3db8aaef1a0cffa6476a8fdf68d10df161f79170
JeusT0910 <72457937+JeusT0910@users.noreply.github.com>
2020-12-14 20:23:21 +0700

    lấy loại sản phẩm từ trang home

--- DoAnAsp/Controllers/CategoryController.cs
+++ DoAnAsp/Controllers/CategoryController.cs
@@ -6,14 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DoAnAsp.Models;
 using System.Diagnostics;
+using DoAnAsp.Areas.Admin.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnAsp.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly DPContext _context;
+
+        public CategoryController(DPContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if(id!=null)
+            {
+                var ds = await _context.sanpham.ToListAsync();
+                var dss = ds.Where(x => x.IdLSP == id);
+                return View(dss);
+            }
+            else
+            {
+                return View(await _context.sanpham.ToListAsync());
+            }
+            
+
+            
         }
     }
 }
